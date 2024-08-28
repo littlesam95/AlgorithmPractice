7b73565673e9e126f1eefcd2ca81b7b32b00f55d
littlesam95 <55424662+littlesam95@users.noreply.github.com>
2024-08-29 08:25:59 +0900

    [Bronze III] Title: 별 찍기 - 9, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2446. 별 찍기 － 9/별 찍기 － 9.cc
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#define FASTIO cin.tie(NULL); cout.tie(NULL); ios::sync_with_stdio(false);
+
+using namespace std;
+int N;
+
+void Input() {
+	cin >> N;
+}
+
+void Find_Answer() {
+	for (int i = N; i > 0; i--) {
+		for (int j = 0; j < (N - i); j++) {
+			cout << " ";
+		}
+		for (int j = 0; j < (i - 1); j++) {
+			cout << "*";
+		}
+		cout << "*";
+		for (int j = 0; j < (i - 1); j++) {
+			cout << "*";
+		}
+		cout << "\n";
+	}
+	for (int i = 2; i <= N; i++) {
+		for (int j = 0; j < (N - i); j++) {
+			cout << " ";
+		}
+		for (int j = 0; j < (i - 1); j++) {
+			cout << "*";
+		}
+		cout << "*";
+		for (int j = 0; j < (i - 1); j++) {
+			cout << "*";
+		}
+		cout << "\n";
+	}
+}
+
+int main() {
+	FASTIO
+	Input();
+	Find_Answer();
+
+	return 0;
+}
