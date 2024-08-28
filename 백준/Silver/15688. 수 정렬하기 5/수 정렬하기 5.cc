8f4080a6143982b1d21990178d5be76e1ff352e7
littlesam95 <55424662+littlesam95@users.noreply.github.com>
2024-08-28 11:08:22 +0900

    [Silver V] Title: 수 정렬하기 5, Time: 4748 ms, Memory: 5932 KB -BaekjoonHub

---
+++ 백준/Silver/15688. 수 정렬하기 5/수 정렬하기 5.cc
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#define FASTIO cin.tie(0); cout.tie(0); ios::sync_with_stdio(false);
+
+using namespace std;
+int N;
+vector<int> Vec;
+
+void input() {
+    cin >> N;
+    Vec.resize(N);
+    for (int i = 0; i < N; i++) {
+        cin >> Vec[i];
+    }
+}
+
+void settings() {
+    sort(Vec.begin(), Vec.end());
+}
+
+void find_Answer() {
+    for (int i = 0; i < N; i++) {
+        cout << Vec[i] << "\n";
+    }
+}
+
+int main() {
+    FASTIO
+    input();
+    settings();
+    find_Answer();
+
+    return 0;
+}
