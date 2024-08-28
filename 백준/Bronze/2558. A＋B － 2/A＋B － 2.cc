82db2c804916e2a80606be801a39e6e9b4474b6c
littlesam95 <55424662+littlesam95@users.noreply.github.com>
2024-08-29 08:30:05 +0900

    [Bronze V] Title: A+B - 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2558. A＋B － 2/A＋B － 2.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <vector>
+#include <algorithm>
+#define FIRST cin.tie(NULL); cout.tie(NULL); ios::sync_with_stdio(false);
+
+using namespace std;
+int A, B;
+
+int main() {
+	FIRST;
+    cin >> A >> B;
+    cout << A + B << "\n";
+    
+	return 0;
+}
