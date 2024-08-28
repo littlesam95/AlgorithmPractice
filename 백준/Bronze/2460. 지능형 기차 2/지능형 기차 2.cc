492715e4fbc2d44cb3ba3310333e15fab7f8dcd9
littlesam95 <55424662+littlesam95@users.noreply.github.com>
2024-08-29 08:26:40 +0900

    [Bronze III] Title: 지능형 기차 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2460. 지능형 기차 2/지능형 기차 2.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#define FASTIO cin.tie(NULL); cout.tie(NULL); ios::sync_with_stdio(false);
+
+using namespace std;
+int People = 0;
+int Answer = 0;
+
+void Input() {
+	for (int i = 0; i < 10; i++) {
+		int A, B;
+		cin >> A >> B;
+		People += (B - A);
+		Answer = max(Answer, People);
+	}
+}
+
+void Find_Answer() {
+	cout << Answer << "\n";
+}
+
+int main() {
+	FASTIO
+	Input();
+	Find_Answer();
+
+	return 0;
+}
