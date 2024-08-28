503f8e22e501e83cf3a76e54ed65a1aac0bced64
littlesam95 <55424662+littlesam95@users.noreply.github.com>
2024-08-29 08:20:50 +0900

    [Gold III] Title: 줄 세우기, Time: 28 ms, Memory: 4152 KB -BaekjoonHub

---
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.cc
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <queue>
+#include <algorithm>
+#define MAX 32001
+
+using namespace std;
+int N, M;
+vector<int> vec[MAX];
+int parent_num[MAX];
+queue<int> q;
+vector<int> answer;
+
+void dfs() {
+	while (!q.empty()) {
+		int now_idx = q.front();
+		answer.push_back(now_idx);
+		q.pop();
+
+		for (int i = 0; i < vec[now_idx].size(); i++) {
+			int next_idx = vec[now_idx][i];
+			parent_num[next_idx]--;
+			if (parent_num[next_idx] == 0) {
+				q.push(next_idx);
+			}
+		}
+	};
+}
+
+int main() {
+	cin.tie(NULL);
+	cout.tie(NULL);
+	ios::sync_with_stdio(false);
+
+	cin >> N >> M;
+	for (int i = 0; i < M; i++) {
+		int x, y;
+		cin >> x >> y;
+		parent_num[y]++;
+		vec[x].push_back(y);
+	}
+	for (int i = 1; i <= N; i++) {
+		if (parent_num[i] == 0) {
+			q.push(i);
+		}
+	}
+	dfs();
+	for (int i = 0; i < answer.size(); i++) {
+		cout << answer[i] << " ";
+	}
+
+	return 0;
+}
