f3ed0ea229788e8d916bface30a44ac7590f3cb4
littlesam95 <55424662+littlesam95@users.noreply.github.com>
2024-08-30 08:27:19 +0900

    [Gold III] Title: 별자리 만들기, Time: 0 ms, Memory: 2692 KB -BaekjoonHub

---
+++ 백준/Gold/4386. 별자리 만들기/별자리 만들기.cc
@@ -0,0 +1,75 @@
+#include <iostream>
+#include <string>
+#include <string.h>
+#include <vector>
+#include <queue>
+#include <cmath>
+#include <climits>
+#include <algorithm>
+#define MAX 100001
+
+using namespace std;
+int N, M;
+int Edge_size = 0;
+int parent[MAX];
+vector<pair<double, double> > Pos;
+vector<pair<double, pair<int, int> > > vec;
+double answer = 0;
+
+void init() {
+	for (int i = 1; i <= N; i++) {
+		parent[i] = -1;
+	}
+}
+
+int Find(int X) {
+	if (parent[X] < 0) {
+		return X;
+	}
+	return parent[X] = Find(parent[X]);
+}
+
+void Union_Set(int X, int Y) {
+	int P_X = Find(X);
+	int P_Y = Find(Y);
+	if (P_X != P_Y) {
+		parent[P_Y] = P_X;
+	}
+}
+
+int main() {
+	cin.tie(NULL);
+	ios::sync_with_stdio(false);
+
+	cin >> N;
+	Pos.resize(N + 1);
+	for (int i = 1; i <= N; i++) {
+		cin >> Pos[i].first >> Pos[i].second;
+	}
+	for (int i = 1; i <= N; i++) {
+		for (int j = i + 1; j <= N; j++) {
+			int A = i;
+			int B = j;
+			double X = (Pos[i].first - Pos[j].first) * (Pos[i].first - Pos[j].first);
+			double Y = (Pos[i].second - Pos[j].second) * (Pos[i].second - Pos[j].second);
+			double C = sqrt(X + Y);
+			vec.push_back(make_pair(C, make_pair(A, B)));
+		}
+	}
+	sort(vec.begin(), vec.end());
+	init();
+	for (int i = 0; i < vec.size(); i++) {
+		int A = vec[i].second.first;
+		int B = vec[i].second.second;
+		int C = vec[i].first;
+		if (Find(A) != Find(B)) {
+			Union_Set(A, B);
+			answer += vec[i].first;
+		}
+	}
+	cout << fixed;
+	cout.precision(2);
+	cout << round(answer * 100) / (double)100 << "\n";
+
+	return 0;
+}
