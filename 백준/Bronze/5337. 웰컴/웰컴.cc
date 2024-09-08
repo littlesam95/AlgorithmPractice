455bbc8a49c676450bce06f85d8446760f9b7628
littlesam95 <55424662+littlesam95@users.noreply.github.com>
2024-09-09 08:06:29 +0900

    [Bronze V] Title: 웰컴, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5337. 웰컴/웰컴.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <unordered_set>
+#include <unordered_map>
+#include <bitset>
+#include <stack>
+#include <queue>
+#include <vector>
+#include <algorithm>
+#include <climits>
+#define FASTIO cin.tie(NULL); cout.tie(NULL); ios::sync_with_stdio(false);
+#define MAX 51
+#define LL long long
+#define INF 1e9
+
+using namespace std;
+
+void Find_Answer() {
+	cout << ".  .   .\n";
+	cout << "|  | _ | _. _ ._ _  _\n";
+	cout << "|/\\|(/.|(_.(_)[ | )(/.\n";
+}
+
+int main() {
+	FASTIO
+	Find_Answer();
+
+	return 0;
+}
