99c8a2a57e72e50204d93eef76d2d254277b0983
littlesam95 <55424662+littlesam95@users.noreply.github.com>
2024-09-09 08:06:43 +0900

    [Bronze V] Title: 콜센터, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5339. 콜센터/콜센터.cc
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <unordered_set>
+#include <unordered_map>
+#include <bitset>
+#include <ctime>
+#include <stack>
+#include <queue>
+#include <vector>
+#include <algorithm>
+#include <climits>
+#define FASTIO cin.tie(NULL); cout.tie(NULL); ios::sync_with_stdio(false);
+#define MAX 101
+#define LL long long
+#define INF 1e9
+
+using namespace std;
+
+void Find_Answer() {
+	cout << "     /~\\\n";
+	cout << "    ( oo|\n";
+	cout << "    _\\=/_\n";
+	cout << "   /  _  \\\n";
+	cout << "  //|/.\\|\\\\\n";
+	cout << " ||  \\ /  ||\n";
+	cout << "============\n";
+	cout << "|          |\n";
+	cout << "|          |\n";
+	cout << "|          |\n";
+}
+
+int main() {
+	FASTIO
+	Find_Answer();
+
+	return 0;
+}
