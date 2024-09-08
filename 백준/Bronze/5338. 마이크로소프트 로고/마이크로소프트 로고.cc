06075c4a4690ac1c01e741b4f53420e2c3f62205
littlesam95 <55424662+littlesam95@users.noreply.github.com>
2024-09-09 08:06:36 +0900

    [Bronze V] Title: 마이크로소프트 로고, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5338. 마이크로소프트 로고/마이크로소프트 로고.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <vector>
+#include <algorithm>
+#define FIRST cin.tie(NULL); cout.tie(NULL); ios::sync_with_stdio(false);
+
+using namespace std;
+
+int main() {
+	FIRST;
+	cout << "       _.-;;-._" << "\n";
+	cout << "\'-..-\'|   ||   |" << "\n";
+	cout << "\'-..-\'|_.-;;-._|" << "\n";
+	cout << "\'-..-\'|   ||   |" << "\n";
+	cout << "\'-..-\'|_.-''-._|" << "\n";
+
+	getchar();
+	getchar();
+	return 0;
+}
